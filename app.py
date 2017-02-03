b7a845cdcc21c4a10e02c5c6a1a1c0f8de956793
Pierre Quentel <pierre.quentel@orange.com>
2017-02-03 16:56:51 +0100

    debug

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    application.run(host=os.environ['DEV_SERVICE_HOST'],
+    application.run(host='0.0.0.0',
         port=os.environ['DEV_SERVICE_PORT'])
